5056572506c65bf5f8eb0f0c39764b9c8633be48
Ramon Diego <ramondiego857@gmail.com>
2021-11-20 12:08:09 -0300

    Usando "imagens"/"divisória"

---
+++ MapsTest/custom-maps/image-map.js
@@ -0,0 +1,24 @@
+var map;
+        function initMap() {
+            var mapOptions = {
+                    center: {lat: -34.397, lng: 150.644},
+                    zoom: 8,
+                    mapTypeControlOptions: {
+                        mapTypeIds: ['osm']
+                    }
+            }
+
+            map = new google.maps.Map(document.getElementById('map'),mapOptions)   
+
+            //ImageMapType
+            var imageMapType = new google.maps.ImageMapType({
+                tileSize: new google.maps.Size(256,256) ,
+                getTileUrl: function(coord, zoom) {
+                    return 'https://tile.openstreetmap.org/' + zoom + '/' + coord.x + '/' + coord.y + '.png';
+                },
+                maxZoom: 18,
+                name:'Open Street Map'
+            });
+            map.mapTypes.set('osm', imageMapType);
+            map.setMapTypeId('osm')
+        }
