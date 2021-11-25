6b194ccb87b4277fd194e1e713693fe8d717226b
Ramon Diego <ramondiego857@gmail.com>
2021-11-25 18:34:50 -0300

    Especie de placeholder

---
+++ MapsTest/infowindow.js
@@ -0,0 +1,30 @@
+var map;
+var geocoder;
+function initMap() {
+    var mapOptions = {
+            center:{lat:-3.716816, lng: -38.519115},
+            zoom:6,
+    }
+    map = new google.maps.Map(document.getElementById('map'),mapOptions) 
+//adicionar
+var marker = new google.maps.Marker({
+    position:{lat:-3.716816, lng: -38.519115},
+    map: map,
+    title: 'Sua atual localização',
+    // label:'R',
+    icon: href="./img/1.png",
+    animation:google.maps.Animation.BOUNCE,
+    // ou DROP
+    draggable: true
+})
+
+var infoWindow = new google.maps.InfoWindow({
+    content:'<div class="info"><h2>Informações legais</h2></div>',
+    // maxWidth: 200,
+})
+
+marker.addListener('mouseover', ()=> {
+    infoWindow.open(map, marker)
+})
+
+}
---
+++ MapsTest/marcadores.js
@@ -0,0 +1,37 @@
+var map;
+var geocoder;
+function initMap() {
+    var mapOptions = {
+            center:{lat:-3.716816, lng: -38.519115},
+            zoom:6,
+    }
+    map = new google.maps.Map(document.getElementById('map'),mapOptions) 
+//adicionar
+var marker = new google.maps.Marker({
+    position:{lat:-3.716816, lng: -38.519115},
+    map: map,
+    title: 'Sua atual localização',
+    // label:'R',
+    icon: href="./img/1.png",
+    animation:google.maps.Animation.BOUNCE,
+    // ou DROP
+    draggable: true
+})
+
+// map.addListener('click', function(e) {
+//     var clickPosition = e.latLng
+//     new google.maps.Marker({
+//         position:clickPosition,
+//         map: map,
+//         title: 'Sua atual localização',
+//         // label:'R',
+//         icon: href="./img/1.png",
+//         animation:google.maps.Animation.BOUNCE,
+//         // ou DROP
+//         draggable: true
+//     })
+// })
+
+//remover
+// marker.setMap(null)
+}
