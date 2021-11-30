90eabdac4d8b8edab594ca8145fb2b844e13e6de
Ramon Diego <ramondiego857@gmail.com>
2021-11-29 23:57:29 -0300

    Metade tá feito ramon

---
+++ MapsTest/mapaFinal.js
@@ -0,0 +1,244 @@
+var geocoder;
+var map;
+function initMap() {
+    var latlng = new google.maps.LatLng(-26.7694400,-48.6458300);
+    var mapOptions = {
+            center:latlng,
+            zoom:12,
+            disableDefaultUI: true,
+    }
+    map = new google.maps.Map(document.getElementById('map'),mapOptions) 
+    
+    geocoder = new google.maps.Geocoder();
+    
+    var marker = new google.maps.Marker({
+        map: map,
+        title: 'Sua atual localização',
+        icon: href="./img/preto.png",
+        animation:google.maps.Animation.BOUNCE,
+        // ou DROP
+    })
+
+    var marker1 = new google.maps.Marker({
+        position: {lat: -26.7999400,lng: -48.6478300},
+        map: map,
+        title: 'Encanador',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker2 = new google.maps.Marker({
+        position: {lat: -26.9009400,lng: -48.6678300},
+        map: map,
+        title: 'Encanador',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker3 = new google.maps.Marker({
+        position: {lat: -26.7910400,lng: -48.6478900},
+        map: map,
+        title: 'Encanador',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker4 = new google.maps.Marker({
+        position: {lat: -26.7999400,lng: -48.6478300},
+        map: map,
+        title: 'Encanador',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker5 = new google.maps.Marker({
+        position: {lat: -26.8200450,lng: -48.6487090},
+        map: map,
+        title: 'Encanador',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker6 = new google.maps.Marker({
+        position: {lat: -26.8234567,lng: -48.7078300},
+        map: map,
+        title: 'Eletricista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker7 = new google.maps.Marker({
+        position: {lat: -26.8344567,lng: -48.7098300},
+        map: map,
+        title: 'Eletricista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker8 = new google.maps.Marker({
+        position: {lat: -26.7999400,lng: -48.7060300},
+        map: map,
+        title: 'Eletricista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker9 = new google.maps.Marker({
+        position: {lat: -26.7009400,lng: -48.7050300},
+        map: map,
+        title: 'Eletricista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker10 = new google.maps.Marker({
+        position: {lat: -26.7029400,lng: -48.7080300},
+        map: map,
+        title: 'Eletricista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker11 = new google.maps.Marker({
+        position: {lat: -26.7500400,lng: -48.7080350},
+        map: map,
+        title: 'Mecanico',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker12 = new google.maps.Marker({
+        position: {lat: -26.7815490,lng: -48.5998300},
+        map: map,
+        title: 'Mecanico',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker13 = new google.maps.Marker({
+        position: {lat: -26.7900490,lng: -48.5999800},
+        map: map,
+        title: 'Mecanico',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker14 = new google.maps.Marker({
+        position: {lat: -26.8087400,lng: -48.6908300},
+        map: map,
+        title: 'Mecanico',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker15 = new google.maps.Marker({
+        position: {lat: -26.8010000,lng: -48.6990300},
+        map: map,
+        title: 'Mecanico',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+    var marker16 = new google.maps.Marker({
+        position: {lat: -26.8799400,lng: -48.6478300},
+        map: map,
+        title: 'Diarista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker17 = new google.maps.Marker({
+        position: {lat: -26.7001000,lng: -48.7660300},
+        map: map,
+        title: 'Diarista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker18 = new google.maps.Marker({
+        position: {lat: -26.8000700,lng: -48.6429000},
+        map: map,
+        title: 'Diarista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker19 = new google.maps.Marker({
+        position: {lat: -26.7299400,lng: -48.6988900},
+        map: map,
+        title: 'Diarista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+
+    var marker20 = new google.maps.Marker({
+        position: {lat: -26.7399400,lng: -48.6990900},
+        map: map,
+        title: 'Diarista',
+        icon: href="./img/1.png",
+        animation:google.maps.Animation.DROP,
+        // ou DROP
+    })
+    
+    marker.setPosition(latlng);
+    
+    const circle = new google.maps.Circle({
+        strokeColor:'#2ED9B8',
+        strokeWeight:2,
+        strokeOpacity:1,
+        fillColor:'#F25A00',
+        fillOpacity:'.10',
+        center: latlng,
+        radius: 10000,
+        map:map,
+    })
+    
+        map.addListener('zoom_changed', function() {
+            const circle = new google.maps.Circle({
+                strokeColor:'#2ED9B8',
+                strokeWeight:2,
+                strokeOpacity:0,
+                fillColor:'#ffffff',
+                fillOpacity:'.01',
+                center: latlng,
+                radius: 10000,
+                map:map,
+            })
+
+        })
+    
+}
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){ // callback de sucesso
+        // ajusta a posição do marker para a localização do usuário
+        marker.setPosition(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+    }, 
+    function(error){ // callback de erro
+       alert('Erro ao obter localização!');
+       console.log('Erro ao obter localização.', error);
+    });
+} else {
+    console.log('Navegador não suporta Geolocalização!');
+}
+$(document).ready(function () {
+    initMap();
+});
