8d22b10e065335691da79094f727c6650b87b4c1
Ramon Diego <ramondiego857@gmail.com>
2021-12-06 18:16:21 -0300

    Não funfaa

---
+++ Chats/projeto-chat/src/App.js
@@ -0,0 +1,13 @@
+import {Layout, Container,SignInButton } from "./Components/Layout"
+import "./App.css";
+function App() {
+  return (
+    <Layout>
+      <Container>
+        <SignInButton>Entrar/Criar conta</SignInButton>
+      </Container>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ Chats/projeto-chat/src/Components/Layout.js
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const Layout = styled.div`
+width: 100vw;
+height:100vh;
+display: flex;
+justify-content:center;`;
+
+
+export const Container = styled.div`
+width: 768px;
+height: 100%;
+background: darkgrey;
+`
+;
+
+export const SignInButton = styled.button`
+font-size: 14px;
+color: white;
+background-color: green;
+`;
